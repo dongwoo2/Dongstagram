b59fdb1ccb48adfe75feee06c4991569eec18fab
dongwoo2 <ieiedong@naver.com>
2024-05-23 16:40:15 +0900

    20240523 모델 데이터베이스 생성하여 화면에 데이터 뿌리기 완료

--- content/views.py
+++ content/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
 # Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id')
+
+        return render(request,"Dongstagram/main.html",{"feed_list":feed_list})
